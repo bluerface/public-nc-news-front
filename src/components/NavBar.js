b444371f4d1f422e3b02c7595f189d8a26eaaf71
bluerface <fallingsilkworm@gmail.com>
2016-12-15 17:00:23 +0000

    Conditionally render sign-in/up or user-icon/logout to the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,21 +9,50 @@ const NavBar = React.createClass({
   },
   render: function () {
     return (
-      <nav className="nav">
-        <Link to='/' className="nav-item">Home</Link>
-        {
-          this.props.topics && this.props.topics.map(function (topic, i) {
-            return <Link to={`/${topic.slug}`} key={i} className='nav-item'>{topic.title}</Link>;
-          })
-        }
+      <nav className='nav'>
+        <div className='nav-left'>
+          <Link to='/' className='nav-item'>Home</Link>
+          {
+            this.props.topics && this.props.topics.map(function (topic, i) {
+              return <Link to={`/${topic.slug}`} key={i} className='nav-item'>{topic.title}</Link>;
+            })
+          }
+        </div>
+
+        <div className='nav-right'>
+          {this.getLoggedInHeader()}
+        </div>
       </nav>
     );
+  },
+  getLoggedInHeader: function () {
+    if (!this.props.user) {
+      return (
+        <span className='nav-item'>
+          <Link to='/signup' className='button'>Sign Up</Link>
+          <Link to='/signin' className='button'>Sign In</Link>
+        </span>
+      );
+    } else {
+      return (
+        <span className='nav-item'>
+          <Link to={`/users/${this.props.user.username}`} className='button is-medium'>
+            <span className='icon'>
+              <img src={this.props.user.avatar_url} height='20px' />
+            </span>
+            <span>{this.props.user.name}</span>
+          </Link>
+          <a href='#' className='button'>Log Out</a>
+        </span>
+      );
+    }
   }
 });
 
 function mapStateToProps (state) {
   return {
-    topics: state.main.topics
+    topics: state.main.topics,
+    user: state.auth.currentUser
   };
 }
 
